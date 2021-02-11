e764f4d0ffc61eaecb88165e150ad7e2e119f0bd
whitehyun <cx_nf@naver.com>
2021-02-11 23:31:10 +0900

    QR코드를 적용하여 테스트 코드 작성

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import time
 import serial
-
+from qrcodes.qrcodes import QRCodes
 
 PORT = "/dev/ttyACM0"   # for test by arduino uno
 
 arduino = serial.Serial(PORT, baudrate=9600, timeout=5)
 
 time.sleep(5)
-
+qr = QRCodes()
 while True:
     try:
-        do_rotate = "T" if len(input("문을 열고싶을 경우 아무거나 입력하세요.")) > 1 else "F"
-        arduino.write(do_rotate.encode())
+        qr_lists = qr.detectQR()
+        for qr_url, _ in qr_lists:
+            arduino.writelines(qr_url.encode())
     except Exception:
         raise Exception
