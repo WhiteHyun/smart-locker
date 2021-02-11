4533388d21fc1b9029d7d5d59695433ae978e308
whitehyun <cx_nf@naver.com>
2021-02-11 20:47:24 +0900

    초음파 거리 계산코드 다시 코딩..

--- src/arduino/locker_test.ino
+++ src/arduino/locker_test.ino
@@ -19,25 +19,24 @@ void setup() {
 }
 
 void loop() {
+    float duration, distance;
+
     // 초음파를 보낸다. 다 보내면 echo가 HIGH 상태로 대기하게 된다.
-    digitalWrite(trig, LOW);
-    digitalWrite(echo, LOW);
-    delayMicroseconds(2);
-    digitalWrite(trig, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trig, LOW);
+    digitalWrite(trigPin, HIGH);
+    delay(10);
+    digitalWrite(trigPin, LOW);
 
     // echoPin 이 HIGH를 유지한 시간을 저장 한다.
-    unsigned long duration = pulseIn(echoPin, HIGH);
+    duration = pulseIn(echoPin, HIGH);
     // HIGH 였을 때 시간(초음파가 보냈다가 다시 들어온 시간)을 가지고 거리를 계산 한다.
-    float distance = ((float)(340 * duration) / 10000) / 2;
+    distance = ((float)(340 * duration) / 10000) / 2;
 
     Serial.print(distance);
     Serial.println("cm");
     // 수정한 값을 출력
     delay(500);
 
-    //
+    // step moter
     if (Serial.available()) {
         char buffer = Serial.read();
         if (buffer == 'T') {
