3be41dc9fa87e7eb9a8d7ebd7d3a1af5b88e1586
whitehyun <cx_nf@naver.com>
2021-02-11 20:26:46 +0900

    output 예쁘게 변경 😁

--- main.py
+++ main.py
@@ -8,11 +8,10 @@ arduino = serial.Serial(PORT, baudrate=9600, timeout=5)
 
 time.sleep(5)
 
-working = True
 while True:
     try:
         do_rotate = "T" if input(
-            "If 1, True. If not, False") == '1' else "F"
+            "If 1, True. If not, False\n>>") == '1' else "F"
         arduino.write(do_rotate.encode())
     except Exception:
         raise Exception
