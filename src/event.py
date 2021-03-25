4cfb3a97fb873595fff3a966c4b1d342f10835d8
WhiteHyun <cx_nf@naver.com>
2021-03-25 15:17:19 +0900

    delivery 락커 화면 구현

--- src/event.py
+++ src/event.py
@@ -1,20 +1,98 @@
 import tkinter as tk
 from tkinter import messagebox
+if __name__ == "__main__" or __name__ == "event":
+    from sql import SQL
+    from custom.tkinter_custom_button import TkinterCustomButton
+
+else:
+    from .sql import SQL
+    from .custom.tkinter_custom_button import TkinterCustomButton
 
 
 class ButtonEvent():
 
-    DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+    DATE_FORMAT = "%Y-%m-%d %H:%M:%S"   # datetime 포맷값
 
-    @staticmethod
-    def show_warning():
+    @classmethod
+    def show_warning(cls):
         """
         새 창으로 이벤트를 발생시킵니다.
         """
         return messagebox.showwarning(title="버튼", message="버튼 이벤트 발생")
 
-    @staticmethod
-    def find_delivery():
+    def __show(frame):
+        """
+        프레임(창)을 띄워줍니다.
+        """
+        frame.tkraise()
+
+    @classmethod
+    def delivery(cls, _self):
+        """
+        맡기기 버튼을 눌렀을 때 발생할 이벤트 함수입니다.
+
+        flow:
+            현재 사물함의 상태를 긁어옴({빨간색, 초록색} 또는 {회색, 파란색}으로 구별할 예정)
+            해당 정보에 대해 사물함 버튼 생성 및 이벤트 구현
+            화면에 전달
+        """
+        stats_sql = SQL("root", "", "10.80.76.63", "SML")
+        result = stats_sql.processDB(
+            "SELECT LCKMngKey, UseYn FROM LCKStat ORDER BY LCKMngKey ASC;")
+        cls.__make_button(_self, result)
+        cls.show_frame(_self)
+
+    def __make_button(cls, _self, result):
+        """
+        쿼리로 가져온 결과값을 가지고 버튼을 생성하여 나타냅니다.
+        """
+        from PIL import Image, ImageTk
+
+        # TODO: 무조건 경로 수정해야함!!!
+        play_image = ImageTk.PhotoImage(Image.open(
+            "src/img/lockers.png").resize((60, 60)))
+        for data in result:
+            if data["UseYn"] == 0:
+                _self.buttons.append(TkinterCustomButton(master=_self,
+                                                         bg_color=None,
+                                                         fg_color="#1E8449",
+                                                         border_color=None,
+                                                         hover_color="#2ECC71",
+                                                         image=play_image,
+                                                         corner_radius=10,
+                                                         border_width=0,
+                                                         width=100,
+                                                         height=100,
+                                                         hover=True,
+                                                         command=cls.show_warning))
+            else:
+                _self.buttons.append(TkinterCustomButton(master=_self,
+                                                         bg_color=None,
+                                                         fg_color="#A93226",
+                                                         border_color=None,
+                                                         hover_color="#CD6155",
+                                                         image=play_image,
+                                                         corner_radius=10,
+                                                         border_width=0,
+                                                         width=100,
+                                                         height=100,
+                                                         hover=True,
+                                                         command=cls.show_warning))
+        relx = 0.33
+        rely = 0.2
+        for button in _self.buttons:
+            button.place(relx=relx, rely=rely, anchor=tk.CENTER)
+            relx = 0.66 if relx == 0.33 else 0.33
+            rely = rely + 0.2 if relx == 0.66 else rely
+
+    @classmethod
+    def __makeQR(cls):
+        """
+
+        """
+
+    @classmethod
+    def find_delivery(cls):
         """
         택배를 찾으려고 할 때 발생하는 이벤트
         """
--- src/ui.py
+++ src/ui.py
@@ -29,7 +29,7 @@ class App(tk.Tk):
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        for F in (StartPage, DelieveryPage, FindPage):
+        for F in (StartPage, DeliveryPage, FindPage):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
             self.frames[page_name] = frame
@@ -41,7 +41,7 @@ class App(tk.Tk):
 
         self.show_frame("StartPage")
 
-    def show_frame(self, page_name):
+    def get_frame(self, page_name):
         """
         다른 창을 맨 앞으로 가져오게 하는 메서드
 
@@ -53,8 +53,7 @@ class App(tk.Tk):
             # tkinter frame에서 StartPage Frame이 화면에 보여짐
 
         """
-        frame = self.frames[page_name]
-        frame.tkraise()
+        return self.frames[page_name]
 
 
 class StartPage(tk.Frame):
@@ -69,62 +68,76 @@ class StartPage(tk.Frame):
                          font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
 
-        button1 = TkinterCustomButton(master=self,
-                                      bg_color=None,
-                                      fg_color="#2874A6",
-                                      hover_color="#5499C7",
-                                      text_font=None,
-                                      text="맡기기",
-                                      text_color="white",
-                                      corner_radius=10,
-                                      width=120,
-                                      height=45,
-                                      hover=True,
-                                      #   command=lambda: controller.show_frame("DelieveryPage")
-                                      command=lambda: messagebox.showwarning(title="맡기기 버튼",
-                                                                             message="맡기기 버튼 이벤트 발생")
-                                      )
-        button2 = TkinterCustomButton(master=self,
-                                      bg_color=None,
-                                      fg_color="#2874A6",
-                                      hover_color="#5499C7",
-                                      text_font=None,
-                                      text="찾기",
-                                      text_color="white",
-                                      corner_radius=10,
-                                      width=120,
-                                      height=45,
-                                      hover=True,
-                                      #   command=lambda: controller.show_frame("FindPage")
-                                      command=ButtonEvent.find_delivery
-                                      )
-        button3 = TkinterCustomButton(master=self,
-                                      bg_color=None,
-                                      fg_color="#2874A6",
-                                      hover_color="#5499C7",
-                                      text_font=None,
-                                      text="종료",
-                                      text_color="red",
-                                      corner_radius=10,
-                                      width=120,
-                                      height=45,
-                                      hover=True,
-                                      #   command=lambda: controller.show_frame("FindPage")
-                                      command=lambda: controller.destroy()
-                                      )
-        button1.place(relx=0.33, rely=0.2, anchor=tk.CENTER)
-        button2.place(relx=0.66, rely=0.2, anchor=tk.CENTER)
-        button3.place(relx=0.50, rely=0.3, anchor=tk.CENTER)
+        self.delivery_button = TkinterCustomButton(master=self,
+                                                   bg_color=None,
+                                                   fg_color="#2874A6",
+                                                   hover_color="#5499C7",
+                                                   text_font=None,
+                                                   text="맡기기",
+                                                   text_color="white",
+                                                   corner_radius=10,
+                                                   width=120,
+                                                   height=45,
+                                                   hover=True,
+                                                   command=lambda: ButtonEvent.delivery(
+                                                       controller.get_frame("DeliveryPage"))
+                                                   )
+        self.find_delivery_button = TkinterCustomButton(master=self,
+                                                        bg_color=None,
+                                                        fg_color="#2874A6",
+                                                        hover_color="#5499C7",
+                                                        text_font=None,
+                                                        text="찾기",
+                                                        text_color="white",
+                                                        corner_radius=10,
+                                                        width=120,
+                                                        height=45,
+                                                        hover=True,
+                                                        command=lambda: controller.show_frame(
+                                                            "FindPage")
+                                                        )
+        self.exit_button = TkinterCustomButton(master=self,
+                                               bg_color=None,
+                                               fg_color="#922B21",
+                                               border_color="white",
+                                               hover_color="#CD6155",
+                                               text_font=None,
+                                               text="tkinter 종료",
+                                               text_color="white",
+                                               corner_radius=10,
+                                               border_width=2,
+                                               width=150,
+                                               height=45,
+                                               hover=True,
+                                               command=lambda: controller.destroy()
+                                               )
+        self.delivery_button.place(relx=0.33, rely=0.2, anchor=tk.CENTER)
+        self.find_delivery_button.place(relx=0.66, rely=0.2, anchor=tk.CENTER)
+        self.exit_button.place(relx=0.50, rely=0.3, anchor=tk.CENTER)
 
 
-class DelieveryPage(tk.Frame):
+class DeliveryPage(tk.Frame):
+    """
+    맡기기 버튼을 눌렀을 때 보여지는 프레임입니다.
+    사물함의 위치 및 상태가 gui로 보여집니다.
+    """
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        label = tk.Label(self, text="This is page 1",
+        label = tk.Label(self, text="택배를 넣을 함을 선택해주세요.",
                          font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
+
+        from PIL import Image, ImageTk
+
+        # TODO: 무조건 경로 수정해야함!!!
+        play_image = ImageTk.PhotoImage(Image.open(
+            "src/img/lockers.png").resize((60, 60)))
+
+        # TODO: 사물(택배)함 버튼 구현
+        self.buttons = []
+
         button = tk.Button(self, text="Go to the start page",
                            command=lambda: controller.show_frame("StartPage"))
         button.pack()
