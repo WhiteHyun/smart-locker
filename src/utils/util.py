29c04c25b5c43c5195240eb66ac9fe20f6c9a070
WhiteHyun <cx_nf@naver.com>
2021-04-09 16:50:22 +0900

    float를 int로 형변환

--- src/utils/util.py
+++ src/utils/util.py
@@ -14,8 +14,8 @@ def success_message(root_view, width=290, height=150):
 
     sw = root_view.winfo_screenwidth()
     sh = root_view.winfo_screenheight()
-    x = (sw - width)/2
-    y = (sh - height)/2
+    x = (sw - width) // 2
+    y = (sh - height) // 2
     top = tk.Toplevel(width=width, height=height)
     top.geometry(f"{width}x{height}+{x}+{y}")
     tk.Message(top, text="완료되었습니다.", padx=20, pady=20).pack()
