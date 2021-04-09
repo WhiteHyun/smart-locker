68c9beb38fa579ae72e199da300bb4f9eaae9938
WhiteHyun <cx_nf@naver.com>
2021-04-09 16:52:25 +0900

    성공메시지: 폰트 설정

--- src/utils/util.py
+++ src/utils/util.py
@@ -18,5 +18,5 @@ def success_message(root_view, width=290, height=150):
     y = (sh - height) // 2
     top = tk.Toplevel(width=width, height=height)
     top.geometry(f"{width}x{height}+{x}+{y}")
-    tk.Message(top, text="완료되었습니다.", padx=20, pady=20).pack()
+    tk.Message(top, text="완료되었습니다", font=root_view.title_font).pack()
     top.after(4000, top.destroy)
