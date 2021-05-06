ccbead3e9ee843657f5da898c13b02f8c3c2cbf9
whitehyun <cx_nf@naver.com>
2021-05-06 23:01:04 +0900

    while True test

--- src/utils/discriminate.py
+++ src/utils/discriminate.py
@@ -13,18 +13,18 @@ class Discriminate:
         self.sql = SQL("root", "", "10.80.76.63", "SML")
 
     def check_door(self):
-        try:
-            with open("data/information.json") as f:
-                import json
-                json_object = json.load(f)
-                # json을 읽어 CRRMngKey 값을 리스트 형태로 저장
-                locker_list = list(
-                    map(lambda x: x["CRRMngKey"], json_object["CRRInfo"]))
-                for locker_key in locker_list:
-                    data = self.sql.processDB(
-                        f"SELECT LIG, HAL FROM SensorValue WHERE CRRMngKey='{locker_key}' LIMIT 1;")
-                    print(data)
+        while True:
+            try:
+                with open("data/information.json") as f:
+                    import json
+                    json_object = json.load(f)
+                    # json을 읽어 CRRMngKey 값을 리스트 형태로 저장
+                    locker_list = list(
+                        map(lambda x: x["CRRMngKey"], json_object["CRRInfo"]))
+                    for locker_key in locker_list:
+                        data = self.sql.processDB(
+                            f"SELECT LIG, HAL FROM SensorValue WHERE CRRMngKey='{locker_key}' LIMIT 1;")
+                        print(data)
 
-        except Exception as e:
-            print(e)
-            return
+            except Exception as e:
+                print(e)
