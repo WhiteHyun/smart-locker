e22bc4d4c4a6aa457c93466534ce8dc509eae1f4
whitehyun <cx_nf@naver.com>
2021-05-11 16:56:21 +0900

    물건 존재 판별여부 수정

--- src/utils/discriminate.py
+++ src/utils/discriminate.py
@@ -48,7 +48,7 @@ class Discriminate:
                 return
             else:
                 data = data[0]
-                if data["SSO"] < 25 or data["FSR"] > 100:
+                if data["SSO"] < 25 or data["FSR"] > 160:
                     return True
                 else:
                     return False
