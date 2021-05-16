c487c49bbd2a7ea5b8ff78e0db502c1c546a40d7
whitehyun <cx_nf@naver.com>
2021-05-16 16:54:08 +0900

    관리자페이지 이동트릭 생성

--- src/page/start_page.py
+++ src/page/start_page.py
@@ -14,14 +14,14 @@ class StartPage(tk.Frame):
     def __init__(self, parent, controller, bg, *args, **kwargs):
         super().__init__(parent)
         self.controller = controller
-
+        self.count = 0
         canvas = tk.Canvas(self, width=controller.width,
                            height=controller.height, bg=bg)
         canvas.pack(fill="both", expand=True)
 
         canvas.create_text(controller.width/2, controller.height/7,
-                           text="택배 보관함", font=controller.title_font, fill="#385ab7")
-
+                           text="택배 보관함", font=controller.title_font, fill="#385ab7", tags="admin")
+        canvas.tag_bind("admin", '<ButtonPress-1>', self.__go_to_admin_page)
         man_img = ImageTk.PhotoImage(Image.open(
             "../img/delivery-man.png" if __name__ == "__main__" or __name__ == "start_page" else "src/img/delivery-man.png"
         ).resize((int(controller.width/5/1.618), int(controller.height/3/1.8))))
@@ -50,3 +50,10 @@ class StartPage(tk.Frame):
                       "FindPage", frame=self
                   )
                   ).place(relx=0.67, rely=0.5, anchor=tk.CENTER)
+
+    def __go_to_admin_page(self, event):
+        """관리자페이지로 이동하는 일종의 트릭함수입니다.
+        """
+        self.count += 1
+        if self.count == 5:
+            self.controller.show_frame("AdminPage", frame=self)
