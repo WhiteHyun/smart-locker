bdb63771af9e3ceac6bffc062e4f51462c9ca8bf
SeungHyeon Hong <57972338+WhiteHyun@users.noreply.github.com>
2021-05-17 00:01:44 +0900

    사물함 크기 조정 (#41)
    * 사물함 버튼 크기 수정
    
    * 사물함 크기 폰트 설정
    
    * label text를 canvas->create_text로 변경
    
    * 사물함 크기 수정
    
    * 맡기기 페이지: 사물함 위치 수정

--- src/page/locker_frame.py
+++ src/page/locker_frame.py
@@ -22,7 +22,7 @@ class LockerFrame(tk.Frame):
         self.controller = controller
         self.page = page
         self.mode = mode
-
+        self.size = None
         self.color_dict = {
             f"{self.STATE_WAIT}": ("#A93226", "#CD6155") if self.page == "FindPage" else ("#385ab7", "#496bc9") if self.mode == self.UNLOCK_MODE else ("#1E8449", "#2ECC71"),
             f"{self.STATE_USED}": ("#1E8449", "#2ECC71") if self.page == "FindPage" else ("#385ab7", "#496bc9") if self.mode == self.UNLOCK_MODE else ("#A93226", "#CD6155"),
@@ -43,6 +43,7 @@ class LockerFrame(tk.Frame):
                 locker_list = sorted(
                     json_object["CRRInfo"], key=lambda dic: dic["location"]["start"]["row"]
                 )
+                self.size = (json_object["LCKSize"]["width"], json_object["LCKSize"]["height"])
                 for json_data in locker_list:
                     self.__make_locker_button(json_data)
         except Exception as e:
@@ -61,10 +62,13 @@ class LockerFrame(tk.Frame):
             빨간색의 사물(택배)함 버튼이 만들어지며 누를 경우 사용할 수 없다는 경고창이 발생합니다.
             초록색의 사물(택배)함 버튼이 만들어지며 누를 경우 사용관련 창으로 넘어갑니다.
         """
-
+        locker_width, locker_height = self.size
+        img_size = 170 -50*(max(locker_height, locker_width)-1)
+        button_size = 250 - 50*(max(locker_width, locker_height)-1)
+        text_font = tkfont.Font(family="a시월구일1", size=20-2*(max(locker_width, locker_height)-1), weight="bold")
         locker_image = ImageTk.PhotoImage(Image.open(
             "../img/lockers.png" if __name__ == "__main__" or __name__ == "locker_frame" else "src/img/lockers.png"
-        ).resize((60, 60)))
+        ).resize((img_size, img_size)))
         location = json_data["location"]
         width = location["width"]
         height = location["height"]
@@ -95,8 +99,9 @@ class LockerFrame(tk.Frame):
                            border_width=1,
                            corner_radius=10,
                            text=locker_number,
-                           width=100*width,
-                           height=100*height,
+                           text_font=text_font,
+                           width=button_size*width,
+                           height=button_size*height,
                            command=decide_function()
                            )
         button.grid(row=location["start"]["row"], column=location["start"]
