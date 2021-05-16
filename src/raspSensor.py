703d108c3b585358ce30fc350c5bb96300a6f059
sungkyulh <sungkyulh@naver.com>
2021-05-16 22:17:48 +0900

    인체감지, 소리용 베이스

--- src/raspSensor.py
+++ src/raspSensor.py
@@ -1,13 +1,18 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BCM)
 
-pirpin = 7
-GPIO.setup(pirpin,GPIO.IN,GPIO.PUD_UP)
 
-while True:
-    if GPIO.input(pirpin) == GPIO.LOW:
-        print('1111')
-    else:
-        print('2222')
-    time.sleep(0.2)
+class DetectMotion:
+
+    def __init__(self) -> None:
+        GPIO.setmode(GPIO.BCM)
+        self.pirpin = 7
+        GPIO.setup(self.pirpin,GPIO.IN,GPIO.PUD_UP)
+    
+
+    def wait_for_motion(self):
+
+        while True:
+            if GPIO.input(self.pripin) == GPIO.LOW:
+                return True
+            time.sleep(0.2)
