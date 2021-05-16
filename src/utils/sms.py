4078ee6a58bdc2cf8fcac606995b288dc8738f29
whitehyun <cx_nf@naver.com>
2021-05-16 17:31:44 +0900

    sms 설정

--- src/utils/sms.py
+++ src/utils/sms.py
@@ -8,7 +8,8 @@ class SMS():
     """
     sms 발송을 위한 class
     """
-    def __init__(self,to,text,imagePath):
+
+    def __init__(self, to, text, imagePath):
         """
         to : 수신인 번호 "01012345678"
         text : 보낼 메시지
@@ -17,21 +18,22 @@ class SMS():
         self.__api_key = "NCSGCXJCYRFYIHHE"
         self.__api_secret = "7PROMFEBKFN3TXMO7RPKALAWW63DCYK6"
         self.__params = dict()
-        self.__params['type'] = 'mms' # Message type ( sms, lms, mms, ata )
-        self.__params['to'] = to # Recipients Number '01000000000,01000000001'
-        self.__params['from'] = '01026147660' # Sender number
-        self.__params['text'] = text # Message
+        self.__params["subject"] = "[QR코드 발급 안내]"
+        self.__params["type"] = "mms"  # Message type ( sms, lms, mms, ata )
+        self.__params["to"] = to  # Recipients Number "01000000000,01000000001"
+        self.__params["from"] = "01026147660"  # Sender number
+        self.__params["text"] = text  # Message
         self.__params["image"] = imagePath
 
-        self.cool = Message(self.__api_key,self.__api_secret)
+        self.cool = Message(self.__api_key, self.__api_secret)
 
     def sendMessage(self):
         """
         메시지 발송, 성공이면 True, 실패면 False
         """
-        try: 
+        try:
             response = self.cool.send(self.__params)
-            if response['success_count'] == 1:
+            if response["success_count"] == 1:
                 return True
             else:
                 return False
