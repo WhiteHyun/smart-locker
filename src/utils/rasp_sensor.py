2d9c6889908b9095119fb04da3d3a948cd6cffcc
sungkyulh <sungkyulh@naver.com>
2021-05-18 19:08:40 +0900

    인체감지센서, 효과음용 클래스 베이스 생성 (#48)
    * 인체감지, 소리용 베이스 추가
    
    * 오탈자 수정
    
    * 인식값 반대로 설정
    
    * 파일 이동 및 이름 변경
    
    * end of line 설정

---
+++ src/utils/rasp_sensor.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+
+
+class DetectMotion:
+
+    def __init__(self) -> None:
+        GPIO.setmode(GPIO.BCM)
+        self.pirpin = 7
+        GPIO.setup(self.pirpin, GPIO.IN, GPIO.PUD_UP)
+
+    def wait_for_motion(self):
+
+        while True:
+            if GPIO.input(self.pirpin) != GPIO.LOW:
+                return True
+            time.sleep(0.2)
