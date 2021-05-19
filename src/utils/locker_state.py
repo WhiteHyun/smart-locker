3a7ad8e905b98bfe4e74bee238ced4665d2561b9
sungkyulh <sungkyulh@naver.com>
2021-05-19 21:30:39 +0900

    상태 판별 방법 수정 (#51)
    * LCKLog의 상태값을 기준으로 Wait 상태일때의 센서 값을 가져와 문턱값을 계산하여 상태 판단
    
    * 보관할 때 문 열리는 시점, 찾을 때 문 닫히는 시점에 로그 기록하도록
    
    * 관리자 페이지에서 고장, 수리 설정시 LCKLog에 Insert하도록 추가
    
    * 문턱값 로직 파기, 기동후 20초의 값을 이용하여 판단하도록 수정

--- src/utils/locker_state.py
+++ src/utils/locker_state.py
@@ -2,6 +2,7 @@ if __name__ == "__main__" or __name__ == "locker_state":
     from sql import SQL
 else:
     from .sql import SQL
+import numpy as np
 
 
 class LockerState:
@@ -20,20 +21,61 @@ class LockerState:
                 map(lambda x: x["CRRMngKey"], json_object["CRRInfo"]))
         self.locker_list = locker_list
 
+    def calc_threshold(self, CRRMngKey, sensorNmLis):
+        '''각 상태 판별을 위한 문턱값을 계산하여 반환합니다.
+
+        Args:
+            CRRMngKey (str): 문턱값을 구할 함 키.
+            sensorNmLis (list): 문턱값을 구하고자 하는 센서 이름. 'LIG','FSR',...
+
+        Return:
+            dict: 구하고자 하는 센서의 문턱값을 딕셔너리 형태로 반환 {센서명:문턱값}
+        '''
+        selectQuery = ''
+        resDict = {}
+
+        try:
+            selectQuery = f"SELECT {', '.join(sensorNmLis)} FROM SensorValue WHERE CRRMngKey = '{CRRMngKey}' ORDER BY SenKey ASC LIMIT 20"
+
+            sensorData = self.sql.processDB(selectQuery)
+
+            if not sensorData:
+                # 해당하는 센서값이 하나도 없는경우 -> 있을려나...? 나오면 뭔가 잘못된경우
+                pass
+            else:
+                for senNm in sensorNmLis:
+                    tmpLis = [d[senNm] for d in sensorData]
+                    resDict[senNm] = np.mean(tmpLis)
+
+        except Exception as e:
+            print(e)
+            return
+
+        return resDict
+
     def is_door_open(self, CRRMngKey):
         """`CRRMngKey`의 함의 문 개폐 여부를 확인합니다.
         """
         try:
+            threshold = self.calc_threshold(CRRMngKey, ['LIG'])
+
+            if not threshold:
+                # 뭔가 문제가 있어 문턱값 못구함
+                return
+
             data = self.sql.processDB(
-                f"SELECT LIG, HAL FROM SensorValue WHERE CRRMngKey='{CRRMngKey}' ORDER BY SenKey DESC LIMIT 1;")
+                f"SELECT HAL, LIG FROM SensorValue WHERE CRRMngKey='{CRRMngKey}' ORDER BY SenKey DESC LIMIT 1;")
             if not data:
                 return
+
+            if (data[0]['HAL'] == 0) or (np.abs(data[0]['LIG']-threshold['LIG']) < 20):
+                # 닫혀 있는 케이스
+                # 홀센서가 인식되어있으면 무조건 닫혀있는거
+                # 홀센서가 인식 안되어있더라도 광량이 변화가 없다고 판단되면 닫혀있다고 판단?
+                return False
             else:
-                data = data[0]
-                if data["HAL"] == 1:
-                    return True
-                else:
-                    return False
+                # 닫혀있지 않으면 열려있는걸로
+                return True
         except Exception as e:
             print(e)
             return
@@ -42,16 +84,23 @@ class LockerState:
         """`CRRMngKey`의 함 내부에 물건이 존재하는지 판별하는 함수입니다.
         """
         try:
+
+            threshold = self.calc_threshold(CRRMngKey, ['SSO', 'FSR'])
+
+            if not threshold:
+                # 뭔가 문제가 있어 문턱값 못구함
+                return
+
             data = self.sql.processDB(
                 f"SELECT SSO, FSR FROM SensorValue WHERE CRRMngKey='{CRRMngKey}' ORDER BY SenKey DESC LIMIT 1;")
             if not data:
                 return
+
+            if (threshold['SSO']-data[0]["SSO"]) > 5 or (data[0]["FSR"]-threshold['FSR']) > 10:
+                # 물건이 있을려면 압력이 증가하거나 초음파가 감소하는경우
+                return True
             else:
-                data = data[0]
-                if data["SSO"] < 25 or data["FSR"] > 200:
-                    return True
-                else:
-                    return False
+                return False
         except Exception as e:
             print(e)
             return
