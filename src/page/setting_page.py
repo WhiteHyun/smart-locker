1f4b68351618b6e754ff7c52bd038bb72d1ba071
whitehyun <cx_nf@naver.com>
2021-05-19 16:57:36 +0900

    문 개폐 상태버튼 추가

--- src/page/setting_page.py
+++ src/page/setting_page.py
@@ -33,14 +33,36 @@ class SettingPage(tk.Frame):
         unlock_img = ImageTk.PhotoImage(Image.open(
             "../img/unlock.png" if __name__ == "__main__" or __name__ == "setting_page" else "src/img/unlock.png"
         ).resize((int(100/1.618), int(100/1.618))))
+        open_img = ImageTk.PhotoImage(Image.open(
+            "../img/open.png" if __name__ == "__main__" or __name__ == "setting_page" else "src/img/open.png"
+        ).resize((int(100/1.618), int(100/1.618))))
+        closed_img = ImageTk.PhotoImage(Image.open(
+            "../img/closed.png" if __name__ == "__main__" or __name__ == "setting_page" else "src/img/closed.png"
+        ).resize((int(100/1.618), int(100/1.618))))
 
         self.canvas = tk.Canvas(self, width=controller.width,
                                 height=controller.height, bg=bg)
         self.canvas.pack(fill="both", expand=True)
         self.mode = kwargs["mode"]
+        self.door_state = "O"
         self.canvas.create_text(controller.width/2, controller.height/7,
                                 text="설정 페이지 (관리자)", font=controller.title_font, fill="#385ab7")
+        self.button_group = tk.Frame(master=self)
+        self.open_button = SMLButton(master=self.button_group,
+                                     border_width=1,
+                                     width=200,
+                                     height=100,
+                                     image=open_img,
+                                     command=lambda: self.set_door_state("O"))
+        self.closed_button = SMLButton(master=self.button_group,
+                                       border_width=1,
+                                       width=200,
+                                       height=100,
+                                       image=closed_img,
+                                       command=lambda: self.set_door_state("C"))
 
+        self.open_button.pack(side="left", fill="both", expand=True)
+        self.closed_button.pack(side="right", fill="both", expand=True)
         self.locker_frame = self.__load_locker(self.mode)
 
         SMLButton(master=self,
@@ -72,6 +94,32 @@ class SettingPage(tk.Frame):
                   command=lambda: self.__change_locker(LockerFrame.UNLOCK_MODE)
                   ).place(x=controller.width-180, y=200)
 
+    def set_door_state(self, state):
+        """문 개폐 모드를 설정합니다.
+
+        모드 종류:
+            `Open mode`
+            `Closed mode`
+        """
+        # 기존과 같은 경우에는 단순리턴
+        if self.door_state == state:
+            return
+        # 오픈상태로 바꿀예정인가?
+        if state == "O":
+            self.open_button.configure_color(fg_color="#1E8449",
+                                             hover_color="#2ECC71")
+            self.closed_button.configure_color(fg_color="#A93226",
+                                               hover_color="#CD6155")
+        elif state == "C":
+            self.closed_button.configure_color(fg_color="#1E8449",
+                                               hover_color="#2ECC71")
+            self.open_button.configure_color(fg_color="#A93226",
+                                             hover_color="#CD6155")
+        else:   # dangerous!
+            print("!DANGEROUS! PLESASE FIX ME!!")
+            return
+        self.door_state = state
+
     def set_locker(self, CRRMngKey, current_state, locker_number):
         """함의 고장유무를 설정합니다.
         """
@@ -97,7 +145,7 @@ class SettingPage(tk.Frame):
         assert result is not None
         result = result[0]["SyncSensor"]
         ratch = RatchController.instance()
-        ratch.execute(result, "O")
+        ratch.execute(result, self.door_state)
 
     def __load_locker(self, mode):
         """LockerFrame을 로드합니다.
@@ -115,6 +163,13 @@ class SettingPage(tk.Frame):
         # 기존 모드와 동일한 경우
         if self.mode == mode:
             return
+        # 강제개폐 설정모드일 경우 문 여닫기버튼 띄워줌
+        if mode == LockerFrame.UNLOCK_MODE:
+            self.button_group.place(relx=0.5, rely=0.3, anchor=tk.CENTER)
+        # 고장상태 설정모드일 경우 여닫기 버튼 지워줌
+        elif mode == LockerFrame.FIX_MODE:
+            self.button_group.place_forget()
+
         self.mode = mode
         self.locker_frame.destroy()
         self.locker_frame = self.__load_locker(mode)
